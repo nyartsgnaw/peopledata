207af66ad501145b9fc575201f458a410a7aa867
nyartsgnaw <nyartsgnaw@users.noreply.github.com>
2019-09-10 17:26:43 -0700

    get linkedin links by google search

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # peopledata
+
+install phantonjs for requirements: https://pythonspot.com/selenium-phantomjs/  
+export phantonjs to $PATH
+
+1. run scrape_google.py to get Linkedin URLs
+
+2. run scrape_linkedin.py to get linkedin html
+
---
+++ scrape_linkedin.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+def click_xpath(xpath):
+    element = WebDriverWait(driver, timeout=3, poll_frequency=0.05).until(
+        EC.visibility_of_element_located((By.XPATH, xpath))
+    )
+    element.click()
+
+def fill_id(id, text):
+    element = WebDriverWait(driver, timeout=3, poll_frequency=0.05).until(
+        EC.visibility_of_element_located((By.ID, id))
+    )
+    element.clear()
+    element.send_keys(text)
+
+
+if __name__ == '__main__':
+    driver = webdriver.PhantomJS()
+    driver.get('https://www.linkedin.com/uas/login')
+    linkedin_email = '511949487@qq.com'
+    linkedin_password = 'Wifihi123'
+    fill_id('username', linkedin_email)
+    fill_id('password', linkedin_password)
+    click_xpath('//button[@type="submit"]')
+
+    url = 'https://www.linkedin.com/in/weichao-will-chou-04588832'
+    driver.get(url)
+    soup = BeautifulSoup(driver.page_source, 'lxml')
