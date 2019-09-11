e658dd4f952e0edfe4a4582e0c53e89981fbd6e5
nyartsgnaw <nyartsgnaw@users.noreply.github.com>
2019-09-10 17:58:00 -0700

    Update scrape_links.py

--- scrape_links.py
+++ scrape_links.py
@@ -5,8 +5,17 @@ import numpy as np
 from bs4 import BeautifulSoup
 from selenium import webdriver
  
-def get_urls_from_soup(soup):
-	urls = []
+def get_google_url(search_term, start=False, domain='.com', quotes=False):
+	search_term = search_term.replace(' ', '%20')
+	if quotes:
+		search_term = '%22' + search_term + '%22'
+	ggurl = 'http://www.google' + domain + '/search?q=' + search_term + '&num=100'
+	if start:
+		ggurl = ggurl + '&start=' + str(start)
+	return ggurl
+
+def extract_urls_from_soup(soup):
+    	urls = []
 	links = []
 	tags = ['Cached','Translate this page', 'Similar', 'Account', 'Search', 'Maps', 'YouTube', 'Play', 'News',
 			'Gmail', 'Contacts', 'Drive', 'Calendar', 'Google+', 'Translate', 'Photos', 'More',
@@ -21,38 +30,25 @@ def get_urls_from_soup(soup):
 				links.append(link)
 	urls = [url.split('&')[0] for url in urls if url.startswith('https://www.linkedin.com/in')]
 	return urls
- 
-def get_target_urls(base_url, driver, size_return=400):
+
+def scan_google_pages(base_url, driver, size_return=400):
 	all_urls = []
 	for page_start_id in range(0, 100 * int(np.floor(size_return / 10)), 100):
 		urls = []
 		ggurl = base_url + '&start=' + str(page_start_id)
 		driver.get(ggurl)
 		soup = BeautifulSoup(driver.page_source, 'lxml')
-		urls = get_urls_from_soup(soup)
+		urls = extract_urls_from_soup(soup)
 		if urls:
 			all_urls += urls
 			time.sleep(np.random.randint(5, 7))
 		if not urls or len(all_urls) > size_return:
 			break
 	return all_urls
-
-def get_google_url(search_term, start=False, domain='.com', quotes=False):
-	search_term = search_term.replace(' ', '%20')
-	if quotes:
-		search_term = '%22' + search_term + '%22'
-	ggurl = 'http://www.google' + domain + '/search?q=' + search_term + '&num=100'
-	if start:
-		ggurl = ggurl + '&start=' + str(start)
-	return ggurl
-
-def get_linkedin_urls(search_term, driver, size_return=400):
-	base_url = get_google_url(search_term + ' site:linkedin.com/in',)
-	urls = get_target_urls(base_url, driver, size_return=size_return)
-	return urls
  
 if __name__ == '__main__':
 	driver = webdriver.PhantomJS()
-	urls = get_linkedin_urls('Nankai, Mountainview', driver, size_return=50)
+	base_url = get_google_url('Nankai, Mountainview' + ' site:linkedin.com/in',)
+	urls = scan_google_pages(base_url, driver, size_return=50)
 	print(urls)
 	driver.close()
