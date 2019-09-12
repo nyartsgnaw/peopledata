e2044e4395fabaf66d1d9ef0c9c6c3fc2f57cb0a
nyartsgnaw <nyartsgnaw@users.noreply.github.com>
2019-09-12 00:21:47 -0700

    write linkedin json to local file

--- scrape_linkedin.py
+++ scrape_linkedin.py
@@ -7,13 +7,13 @@ from collections import defaultdict
 import json
 import os
 
-def click_xpath(xpath):
+def click_xpath(xpath, driver):
     element = WebDriverWait(driver, timeout=3, poll_frequency=0.05).until(
         EC.visibility_of_element_located((By.XPATH, xpath))
     )
     element.click()
 
-def fill_id(id, text):
+def fill_id(id, text, driver):
     element = WebDriverWait(driver, timeout=3, poll_frequency=0.05).until(
         EC.visibility_of_element_located((By.ID, id))
     )
@@ -28,23 +28,28 @@ def parse_linkedin_soup(soup):
             data[x['$type']].append(x)
     return data
 
-if __name__ == '__main__':
+def init_driver(linkedin_email='511949487@qq.com', linkedin_password='Wifihi123'):
     driver = webdriver.PhantomJS()
     driver.get('https://www.linkedin.com/uas/login')
-    linkedin_email = '511949487@qq.com'
-    linkedin_password = 'Wifihi123'
-    fill_id('username', linkedin_email)
-    fill_id('password', linkedin_password)
-    click_xpath('//button[@type="submit"]')
-    url = 'https://www.linkedin.com/in/straynwang/'
-    driver.get(url)
-    soup = BeautifulSoup(driver.page_source, 'lxml')
-    data = parse_linkedin_soup(soup)
+    fill_id('username', linkedin_email, driver)
+    fill_id('password', linkedin_password, driver)
+    click_xpath('//button[@type="submit"]', driver)
+    return driver
+
+if __name__ == '__main__':
+    driver = init_driver()
+    input_path = '/tmp/linkedin_profiles/test_google.txt'
     output_path = '/tmp/linkedin_profiles/test.json'
     os.system('mkdir -p {}'.format(os.path.dirname(output_path)))
-    with open(output_path, 'w') as f:
-        json_data = json.dumps(data, indent=4, sort_keys=True)
-        f.write(json_data)
-        print(json_data)
-        print('\n')
-        print('Profile written to local: {} -> {}'.format(url, output_path))
+    with open(output_path, 'w') as write_f:
+        with open(input_path) as f:
+            for line in f:
+                url = line.strip('\n')
+                driver.get(url)
+                soup = BeautifulSoup(driver.page_source, 'lxml')
+                data = parse_linkedin_soup(soup)
+                json_data = json.dumps(data, indent=4, sort_keys=True)
+                write_f.write(json_data + '\n')
+                print(json_data)
+                print('\n')
+                print('Profile written to local: {} -> {}'.format(url, output_path))
