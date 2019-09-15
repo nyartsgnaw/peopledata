f46fabb60f5e7a12d53a3be2bdcf97b807c65012
Nyarts <nyartsgnaw@gmail.com>
2019-09-14 17:46:42 -0700

    Refactor function to class

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # peopledata
 
-install phantonjs for requirements: https://pythonspot.com/selenium-phantomjs/  
+initialize environment:
+```
+# install phantonjs for requirements: https://pythonspot.com/selenium-phantomjs/  
+export PATH=$PATH:$CODEPATH/init_env/phantomjs-2.1.1-linux-x86_64/bin
 
-export phantonjs to $PATH
+# activate peopledata environment
+export CODEPATH=$HOME/peopledata
+export PYTHONPATH=$PYTHONPATH:$CODEPATH/lib/
+
+# install python requirements
+pip install -r $CODEPATH/init_env/requirements.txt
+```
---
+++ scripts/scrape_linkedin.py
@@ -0,0 +1,20 @@
+import json
+import os
+from collections import defaultdict
+from scraper import linkedin_scraper
+
+
+if __name__ == '__main__':
+    linkedin_scraper = linkedin_scraper.LinkedinScraper(linkedin_email='511949487@qq.com', linkedin_password='Wifihi123')
+    input_path = '/tmp/linkedin_profiles/test_google.txt'
+    output_path = '/tmp/linkedin_profiles/test_linkedin.json'
+    os.system('mkdir -p {}'.format(os.path.dirname(output_path)))
+    with open(output_path, 'w') as write_f:
+        with open(input_path) as f:
+            for line in f:
+                url = line.strip('\n')
+                data = linkedin_scraper.get_profile(url)
+                write_f.write(json.dumps(data) + '\n')
+                print(json.dumps(data, indent=4, sort_keys=True))
+                print('\n')
+                print('Profile written to local: {} -> {}'.format(url, output_path))
