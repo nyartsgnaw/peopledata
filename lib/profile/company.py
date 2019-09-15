ab82713172111ab755373f8e889959956a987be7
Nyarts <nyartsgnaw@gmail.com>
2019-09-15 01:24:38 -0700

    Add Linkedin profile structure data examples

---
+++ lib/profile/company.py
@@ -0,0 +1,19 @@
+class Company(self):
+    def __init__(self):
+        self.data = ''
+        pass
+
+    def get_company_name(self):
+        pass
+
+    def get_size(self):
+        pass
+
+    def get_founded_year(self):
+        pass
+
+    def get_funding(self):
+        pass
+
+    def get_industry(self):
+        pass
---
+++ lib/profile/experience.py
@@ -0,0 +1,35 @@
+
+class Experience(self):
+    def __init__(self):
+        pass
+
+    def get_company_name(self):
+        pass
+
+    def get_title(self):
+        pass
+
+    def get_location(self):
+        pass
+
+    def get_start_date(self):
+        pass
+    
+    def get_end_date(self):
+        pass
+
+    def get_duration(self):
+        pass
+
+    def predict_seniority(self):
+        """ inferred from title name """
+        pass
+    
+    def predict_industry(self):
+        """ based on Wikipedia text for the company"""
+        pass
+
+    def predict_company_ranking(self):
+        """ get from crunchbase https://pypi.org/project/python-crunchbase/ """
+        pass
+    
