2e4b01d6e4df936feda4ab17d1a9f7dca0dfe12c
Nyarts <nyartsgnaw@gmail.com>
2019-09-15 01:24:38 -0700

    Add profile structure examples

---
+++ lib/profile/location.py
@@ -0,0 +1,22 @@
+class Location(self):
+    def __init__(self):
+        self.data = ''
+        pass
+    
+    def get_location_data(self, location):
+        """ query Google API """
+        pass
+
+    def get_latlong(self):
+        pass
+
+    def get_country(self):
+        pass
+
+    def get_state(self):
+        pass
+    
+    def get_city(self):
+        pass
+
+    
